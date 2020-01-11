73ffec76aaacdfd044b7cc9251cbe77382ae3d73
voxelv <slippytim@gmail.com>
2020-01-11 13:24:22 -0800

    debugging

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,14 @@
 
 int main()
 {
-    init_log();
+    int a = 8;
+    a++;
+    const char* string = "Hello";
+    for (int i = 0; i < 5; ++i) {
+        const char c = string[i];
+        std::cout << c << std::endl;
+    }
+
     log("Hello World!");
     std::cin.get();
 }
